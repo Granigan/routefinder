0d1601aad05ef5f908b8a9a6bfa6cc705139b662
Tero Tapio <tero.tapio@helsinki.fi>
2020-04-14 01:03:58 +0300

    delete deprecated doodlings

--- src/App.js
+++ src/App.js
@@ -10,8 +10,7 @@ const getRoutesForLine = lineStations =>
   lineStations.reduce((acc, stop, index) => 
     index < lineStations.length - 1 
       ? acc.concat([stop+lineStations[index+1], lineStations[index+1]+stop]) 
-      : acc
-    , [])
+      : acc, [])
 
 const routesAvailable = 
   Array.from(new Set(Object.keys(lines).reduce((acc, color) => 
@@ -21,8 +20,8 @@ const routeExists = (origin, destination) => routesAvailable.includes(origin+des
 
 const findDuration = route =>
   roads.filter(road => 
-    road.mista == route.substring(0,1) && road.mihin == route.substring(1,2) ||
-    road.mihin == route.substring(0,1) && road.mista == route.substring(1,2) )
+    (road.mista === route.substring(0,1) && road.mihin === route.substring(1,2)) ||
+    (road.mihin === route.substring(0,1) && road.mista === route.substring(1,2)) )
     .map(road => road.kesto)[0]
 
 const findColour = route =>
@@ -37,119 +36,22 @@ const findNeighbours = station =>
       route.substring(0,1)===station ? route.substring(1,2) : route.substring(0,1))
   ))
 
-const routeDetails = routesAvailable.map(route => ({
+const routeDetails = routesAvailable.reduce( (acc, route) => ({...acc, 
   [route]: {
     origin: route.substring(0,1), 
     destination: route.substring(1,2,), 
     duration: findDuration(route),
     colours: findColour(route)}
-}))
+}), {})
 
 const stationTracking = stations.map(station => 
   ({[station]: {visited: false, distanceFromOrigin: Number.MAX_SAFE_INTEGER, arrivedFrom: null }}))
 
-
 const stationDetails = stations.map(station => 
   ({[station]: {neighbours: findNeighbours(station)}}))
 
-console.log(stationDetails, stationTracking, routeDetails)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const availableRoadsBetweenStations =
-  stations.reduce((acc, cur) => ({
-    ...acc, [cur]:
-      roads.filter(road => road.mista === cur)
-      .reduce((acc, cur) => ({...acc, [cur.mihin]:cur.kesto}), {})
-  }), 
-  {})
-
-const availableRoadsBetweenStationsReversed = 
-  stations.reduce((acc, cur) => ({
-    ...acc, [cur]:
-      roads.filter(road => road.mihin === cur)
-      .reduce((acc, cur) => ({...acc, [cur.mista]:cur.kesto}), {})
-  }), 
-  {})
-
-const availableRoadsBetweenStationsBothWays = 
-  Object.keys(availableRoadsBetweenStations).reduce((acc, cur) => ({...acc, [cur]:{
-    ...availableRoadsBetweenStations[cur], ...availableRoadsBetweenStationsReversed[cur]
-  }}), {})
-
-const listRoutesPerLine = lineName => {
-  const curRoute = lines[lineName]
-  const maxIndex = curRoute.length
-
-  return curRoute.reduce((acc, cur, index, src) => {
-    if(index < maxIndex - 1) {
-      return {...acc, 
-        [cur]:{[src[index+1]]:[lineName]},
-      }
-    } else {
-      return acc
-    }
-    }, {}
-  )
-}
-
-const listRoutesPerLineReversed = lineName => {
-  const curRoute = lines[lineName]
-  const maxIndex = curRoute.length
-
-  return curRoute.reduce((acc, cur, index, src) => {
-    if(index < maxIndex - 1) {
-      return {...acc, 
-        [src[index+1]]:{[cur]:[lineName]}
-      }
-    } else {
-      return acc
-    }
-    }, {}
-  )
-}
-
-const availableRoutes =
-  stations.reduce((acc, cur) => ({...acc, [cur]:{
-    ...listRoutesPerLine("keltainen")[cur], 
-    ...listRoutesPerLineReversed("keltainen")[cur], 
-    ...listRoutesPerLine("sininen")[cur],
-    ...listRoutesPerLineReversed("sininen")[cur],
-    ...listRoutesPerLine("punainen")[cur],
-    ...listRoutesPerLineReversed("punainen")[cur],
-    ...listRoutesPerLine("vihreä")[cur],
-    ...listRoutesPerLineReversed("vihreä")[cur]
-}}), {})
-
-
-let origin = 'G'
-let destination = 'F'
+let origin = 'A'
+let destination = 'C'
 
 function App() {
   return (
@@ -169,9 +71,9 @@ function App() {
       <p>
         Nopein reitti: 
         {origin === destination ? "Lähtöpiste ja määränpää ovat samat." :
-        routeExists(origin, destination) ? "Suora reitti löytyy!" : "Suoraa reittiä ei ole"}
-        {availableRoutes[origin][destination]} 
-        {availableRoadsBetweenStationsBothWays[origin][destination]} 
+        !routeExists(origin, destination) ? "Suoraa reittiä ei ole" :
+        `Suora reitti löytyy! Reitillä kulkee ${routeDetails[origin+destination].colours} 
+        ja matka kestää ${routeDetails[origin+destination].duration} minuuttia.`}
       </p>
     </div>
   );
