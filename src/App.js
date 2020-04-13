937b391aa0c0749958adffeb58c10128052ef98a
Tero Tapio <tero.tapio@helsinki.fi>
2020-04-13 15:35:38 +0300

    add road finding between stations

--- src/App.js
+++ src/App.js
@@ -4,8 +4,26 @@ import routes from './data/reittiopas.json'
 
 const stops = routes.pysakit
 const roads = routes.tiet
-console.log(roads)
 const lines = routes.linjastot
+
+/* slightly more verbose way of findinig roads between stations
+const availableRoadsBetweenStations = stops.map(stop => ({
+  station: stop,
+  routes: roads.filter(road => road.mista === stop).map(road => road.mihin)
+    .concat(roads.filter(road => road.mihin === stop).map(road => road.mista))
+})).reduce((acc, cur) => ({...acc, [cur.station]:cur.routes}), {})
+*/
+
+const availableRoadsBetweenStations = stops.reduce((acc, cur) => ({...acc, 
+  [cur]:roads
+  .filter(road => road.mista === cur).map(road => road.mihin)
+  .concat(roads.filter(road => road.mihin === cur).map(road => road.mista))}), {})
+
+const availableRoutes = availableRoadsBetweenStations.R
+
+let start = ''
+let finish = ''
+
 
 function App() {
   return (
@@ -13,7 +31,18 @@ function App() {
       <header className="Reittiopas">
       Reittiopas
       </header>
-      Pysäkit: {stops}
+      <p>
+        Pysäkit: {stops}
+      </p>
+      <p>
+        Valitse lähtöpiste:
+      </p>
+      <p>
+        Valitse määränpää:
+      </p>
+      <p>
+        Nopein reitti: {availableRoutes}
+      </p>
     </div>
   );
 }
