96d62e69e19521dfa6ff41b7144c4c7871cb3fa2
Tero Tapio <tero.tapio@helsinki.fi>
2020-04-18 00:19:32 +0300

    separate RouteFinder to a component

--- src/App.js
+++ src/App.js
@@ -1,53 +1,5 @@
-import React, { useState } from 'react'
-import { Container, Button } from '@material-ui/core/'
-import StationButtons from './components/StationButtons'
-import { getStations } from './dataService'
-import { findShortestRoute } from './dijkstra'
-
-const RouteFinder = () => {
-  const [origin, setOrigin] = useState('')
-  const [destination, setDestination] = useState('')
-  const [routeTaken, setRouteTaken] = useState([])
-  const [tripDuration, setTripDuration] = useState('')
-  const [lineOptions, setLineOptions] = useState('')
-
-  return (
-    <Container>
-      <div>
-        <h1 className="Reittiopas">Esdger-reittihaku</h1>
-      </div>
-      <Container>
-        {StationButtons(
-          getStations(),
-          origin,
-          destination,
-          setOrigin,
-          setDestination
-        )}
-      </Container>
-      <div>
-        {tripDuration === ''
-          ? ''
-          : `Nopein reitti on ${routeTaken}, ja kestää ${tripDuration} minuuttia.`}
-        {tripDuration > 0 ? `Pysäkkien väleillä kulkevat: ${lineOptions}` : ''}
-      </div>
-      <div>{`Origin: ${origin} Destination: ${destination}`}</div>
-      <Button
-        onClick={() =>
-          findShortestRoute(
-            origin,
-            destination,
-            setTripDuration,
-            setRouteTaken,
-            setLineOptions
-          )
-        }
-      >
-        Etsi reitti
-      </Button>
-    </Container>
-  )
-}
+import React from 'react'
+import RouteFinder from './components/RouteFinder'
 
 const App = () => <RouteFinder />
 
