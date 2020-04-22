a3004653f2c7d3f732e6953a37a048558985a417
Tero Tapio <tero.tapio@helsinki.fi>
2020-04-22 23:38:43 +0300

    show all lines on route

--- src/components/RouteSpecifics.js
+++ src/components/RouteSpecifics.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import DirectionsBusIcon from '@material-ui/icons/DirectionsBus'
-import SyncAltIcon from '@material-ui/icons/SyncAlt';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
 import { green, yellow, red, blue } from '@material-ui/core/colors'
 import { findColour } from '../dataService'
 
@@ -19,12 +19,8 @@ const LineIcon = (line, key) => {
   }
 }
 
-const createLineOptions = (route) => {
-  return
-}
-
 export const RouteSpecifics = ({ duration, route }) => {
-  if (!duration > 0) return null
+  if (!duration) return null
   const stationToStationRoutes = route.reduce(
     (acc, station, index) =>
       index < route.length - 1 ? acc.concat([station + route[index + 1]]) : acc,
@@ -32,16 +28,23 @@ export const RouteSpecifics = ({ duration, route }) => {
   )
   console.log(stationToStationRoutes)
 
-  const visualisation = stationToStationRoutes.map((stationToStationRoute, index) => (
-    <div>
-      {`${stationToStationRoute.substring(0, 1)}`}
-      <SyncAltIcon size="small" value={stationToStationRoute}/>
-      {`${stationToStationRoute.substring(1, 2)}` + ':'}{' '}
-      {LineIcon(findColour(stationToStationRoute)[0], route + index)}
-    </div>
-  ))
+  const visualisation = stationToStationRoutes.map(
+    (stationToStationRoute, index) => (
+      <div key={index}>
+        {`${stationToStationRoute.substring(0, 1)}`}
+        <ArrowForwardIcon
+          fontSize="small"
+          key={stationToStationRoute + index}
+        />
+        {`${stationToStationRoute.substring(1, 2)}` + ':'}{' '}
+        {findColour(stationToStationRoute).map((lineOnRoute) =>
+          LineIcon(lineOnRoute, stationToStationRoute + lineOnRoute)
+        )}
+      </div>
+    )
+  )
 
-  return duration > 0 ? <div>{visualisation}</div> : null
+  return duration ? <div>{visualisation}</div> : null
 }
 
 export default RouteSpecifics
